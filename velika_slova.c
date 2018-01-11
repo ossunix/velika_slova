1d01d19e37de415bffd1ed81b1c3036e35b8b71c
Glavni\Nikola <nikola@mail>
2018-01-11 08:58:53 +0100

    cleanup

--- velika_slova.c
+++ velika_slova.c
@@ -4,19 +4,20 @@
 int main(int argc, char *argv[])
 {
 	int position = 0;
-	char* word;
+	char *word;
 	word = (char*) malloc (100 * sizeof(char));
-	
+
 	scanf("%s", word);
-	
+
 	while (word[position] != '\0' && word[position] != ' ')
 	{
 		if (word[position] >= 'a' && word[position] <= 'z') 
 		{
 			word[position] = word[position] - 32;
-		}  
+		}
 		position++;
 	}
+
 	printf("%s\n", word);
 	return 0;
 }
